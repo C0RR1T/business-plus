d52287ac32b4ed9e9c8914b0497fc1104a1e666a
Oliver-Was-Taken <olivr.pyro@gmail.com>
2022-01-10 16:14:30 +0100

    Ordnerstruktur + Routing Struktur

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
-import 'bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
-import {Route, Routes} from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import StartPage from './routes/start-page/StartPage';
 
 function App() {
-  return (
-      <>
-          <Routes>
-            <Route path="/" element={}/>
-          </Routes>
-      </>
-  );
+    return (
+        <>
+            <Routes>
+                <Route path='/' element={StartPage} />
+            </Routes>
+        </>
+    );
 }
 
 export default App;
