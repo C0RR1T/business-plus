e6c67e93f32c9afdffac64460049c9ba5247465d
Oliver-Was-Taken <olivr.pyro@gmail.com>
2022-01-16 10:45:34 +0100

    navigation + startpage draft

--- src/routes/start-page/StartPage.tsx
+++ src/routes/start-page/StartPage.tsx
@@ -1,3 +1,63 @@
+import { Button, Card, Col, Container, Row } from 'react-bootstrap';
+import travel from '../../static/travel.jpg';
+import meeting from '../../static/meeting.jpg';
+import { Link } from 'react-router-dom';
+
+import './start-page.css';
+
 export default function StartPage() {
-    return <div>Hallo Corsin</div>;
+    return (
+        <div>
+            <Container className={'start-page__container'}>
+                <Row>
+                    <Col>
+                        <Card>
+                            <Card.Header>
+                                <h5 className={'start-page__card-header'}>
+                                    Geschäftsreisen
+                                </h5>
+                            </Card.Header>
+                            <Card.Img
+                                className={'start-page__card_image'}
+                                src={travel}
+                            />
+                            <Card.Body>
+                                <div>
+                                    Einfach und unkompliziert Ihre
+                                    Geschäftsreisen mit uns buchen
+                                </div>
+                                <Link to={'/travels'}>
+                                    <Button className={'start-page__button'}>
+                                        Geschäftsreise buchen
+                                    </Button>
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+
+                    <Col>
+                        <Card>
+                            <Card.Header className={'start-page__card-header'}>
+                                <h5 className={'start-page__card-header'}>
+                                    Meetings
+                                </h5>
+                            </Card.Header>
+                            <Card.Img src={meeting} />
+                            <Card.Body>
+                                <div>
+                                    Einfach und unkompliziert Ihre
+                                    Geschäftsreisen mit uns buchen
+                                </div>
+                                <Link to={'/meetings'}>
+                                    <Button className={'start-page__button'}>
+                                        Meetingraum buchen
+                                    </Button>
+                                </Link>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
+    );
 }
